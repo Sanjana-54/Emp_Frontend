35f3b9c102578b7cdd3399d53363b780a3f3c20d
Sanjana <sanjanagoud054@gmail.com>
2026-04-24 13:54:24 +0530

    changed UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,13 @@ import { NavLink} from "react-router";
 
 function Header() {
   return (
-    <nav className="flex justify-end text-2xl bg-amber-400 p-3 text-white gap-6 ">
+    <nav className="flex flex-col sm:flex-row sm:justify-between items-center  bg-amber-400 p-4 text-white ">
+     
+      <div className="flex flex-col sm:flex-row gap-3 sm:gap-6 text-sm sm:text-lg">
         <NavLink to="" className={({ isActive }) => (isActive ? "text-blue-700 bg-blue-200 " : "")}>Home</NavLink>
         <NavLink to="create-emp" className={({ isActive }) => (isActive ? "text-blue-700 bg-blue-200 " : "")}>Create Emp</NavLink>
         <NavLink to="list" className={({ isActive }) => (isActive ? "text-blue-700 bg-blue-200 " : "")}>Employees</NavLink>
+      </div>
     </nav>
 
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+
 
 function Home() {
   return (
-    <div className=' text-green-950'>
-      <p className='text-6xl text-center mb-3'>Employee Management</p>
-      <p className='text-4xl mt-2 text-center'>This website contains all the details of Employees</p>
+    <div className='min-h-screen flex flex-col justify-center items-center px-4 text-green-950 text-center'>
+      <h1 className='text-2xl sm:text-4xl md:text-6xl  mb-4'>Employee Management</h1>
+      <p className='text-sm sm:text-lg md:text-2xl max-w-xl'>This website contains all the details of Employees</p>
     </div>
   )
 }
